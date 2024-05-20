aac72b836de7bda3837ee916275050628eaaaee4
DevKafayat <olubukola11th@gmail.com>
2024-05-20 11:13:46 +0100

    updated

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.getElementById('add-btn').addEventListener('click', addTask);
 
 function addTask() {
   const task = document.getElementById('task').value;
-  if (task) {
+  if (!task) {
+    alert("Please enter a task!");
+  } else {
     if (tasks.find((existingTask) => existingTask.text === task)) {
       alert("Task already exists in the tasks list!");
     } else if (doneTasks.find((doneTask) => doneTask.text === task)) {
@@ -18,28 +20,33 @@ function addTask() {
   }
 }
 
+
 function updateTaskList() {
-    const taskList = document.getElementById('tasks');
-    taskList.innerHTML = '';
-    tasks.forEach((task, index) => {
-  const taskElement = document.createElement('li');
-  taskElement.textContent = task.text;
-  const markButton = document.createElement('button');
-  markButton.className = 'icon-button'
-  markButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="rgba( 158, 120, 207, 1)" d="m10 15.586l-3.293-3.293l-1.414 1.414L10 18.414l9.707-9.707l-1.414-1.414z"/></svg>'
-  markButton.addEventListener('click', () => markTask(index));
-  const deleteButton = document.createElement('button');
-  deleteButton.className = 'icon-button'
-  deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="rgba(158, 120, 207, 1)" d="M15 2H9c-1.103 0-2 .897-2 2v2H3v2h2v12c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V8h2V6h-4V4c0-1.103-.897-2-2-2M9 4h6v2H9zm8 16H7V8h10z"/></svg>'
-  deleteButton.addEventListener('click', () => deleteTask(index));
-  const buttonContainer = document.createElement('span');
-  buttonContainer.classList.add('buttonContainer');
-  buttonContainer.appendChild(markButton);
-  buttonContainer.appendChild(deleteButton);
-  taskElement.appendChild(buttonContainer);
-  taskList.appendChild(taskElement);
-});
-    document.getElementById('task-count').textContent = tasks.length;
+  const taskList = document.getElementById('tasks');
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const taskElement = document.createElement('li');
+    taskElement.textContent = task.text;
+    const markButton = document.createElement('button');
+    markButton.className = 'icon-button';
+    markButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="rgba(158, 120, 207, 1)"q d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
+    markButton.addEventListener('click', () => markTask(index));
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'icon-button';
+    deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="rgba(158, 120, 207, 1)" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
+    deleteButton.addEventListener('click', () => {
+      if (confirm('Are you sure you want to delete this task?')) {
+        deleteTaskFromTasks(index);
+      }
+    });
+    const buttonContainer = document.createElement('span');
+    buttonContainer.classList.add('buttonContainer');
+    buttonContainer.appendChild(markButton);
+    buttonContainer.appendChild(deleteButton);
+    taskElement.appendChild(buttonContainer);
+    taskList.appendChild(taskElement);
+  });
+  document.getElementById('task-count').textContent = tasks.length;
 }
 
 function markTask(index) {
@@ -49,8 +56,10 @@ function markTask(index) {
     } else {
       tasks[index].done = true;
       doneTasks.push(tasks[index]);
+      tasks.splice(index, 1); // Remove the task from the tasks array
       updateTaskList();
       updateDoneList();
+      localStorage.setItem('tasks', JSON.stringify(tasks));
       localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
     }
   } else {
@@ -58,14 +67,9 @@ function markTask(index) {
   }
 }
 
-
 function deleteTask(index) {
-  const taskToDelete = tasks[index];
-  tasks.splice(index, 1);
-  doneTasks = doneTasks.filter((task) => task.text !== taskToDelete.text);
-  updateTaskList();
+  doneTasks.splice(index, 1);
   updateDoneList();
-  localStorage.setItem('tasks', JSON.stringify(tasks));
   localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
 }
 
@@ -75,27 +79,42 @@ function updateDoneList() {
   doneTasks.forEach((task, index) => {
     const taskElement = document.createElement('li');
     taskElement.textContent = task.text;
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'icon-button';
+    deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="rgba(158, 120, 207, 1)" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
+    deleteButton.addEventListener('click', () => {
+      if (confirm('Are you sure you want to delete this task?')) {
+        deleteTask(index);
+      }
+    });
+    taskElement.appendChild(deleteButton);
     doneList.appendChild(taskElement);
   });
   document.getElementById('done-count').textContent = doneTasks.length;
 }
 
-// Store data in local storage
-window.addEventListener('beforeunload', () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
+function deleteTaskFromTasks(index) {
+  tasks.splice(index, 1);
+  updateTaskList();
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Store data in local storage//
+window.addEventListener('beforeunload', () => {;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
 });
 
-// Retrieve data from local storage
+// Retrieve data from local storage//
 window.addEventListener('load', () => {
-    const storedTasks = localStorage.getItem('tasks');
-    const storedDoneTasks = localStorage.getItem('doneTasks');
-    if (storedTasks) {
-        tasks = JSON.parse(storedTasks);
-    }
-    if (storedDoneTasks) {
-        doneTasks = JSON.parse(storedDoneTasks);
-    }
-    updateTaskList();
-    updateDoneList();
+  const storedTasks = localStorage.getItem('tasks');
+  const storedDoneTasks = localStorage.getItem('doneTasks');
+  if (storedTasks) {
+    tasks = JSON.parse(storedTasks);
+  }
+  if (storedDoneTasks) {
+    doneTasks = JSON.parse(storedDoneTasks);
+  }
+  updateTaskList();
+  updateDoneList();
 });
